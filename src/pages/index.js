ba414a6def94692bcbe259d4f26fb7563b57883a
Wade Christensen <wade@astuteape.com>
2019-03-20 14:30:03 -0700

    Add styles directory and files

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Link to="/about/">Go About</Link>
     <Link to="/blog/">Go Blog</Link>
   </Layout>
 )
