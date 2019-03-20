0e59422feaef7456d06d67cce063b4ae71192eb2
Wade Christensen <wade@astuteape.com>
2019-03-20 12:33:50 -0700

    Add starters for all site pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,16 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hello, World!</h1>
-    <img src="https://source.unsplash.com/random/400x200" alt="Random from unspash" />
+    <img
+      src="https://source.unsplash.com/random/400x200"
+      alt="Random from unspash"
+    />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/blog/">Go Blog</Link>
   </Layout>
 )
 
