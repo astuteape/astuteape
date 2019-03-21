411c6dc7dc9553660d7cd1aa2872b1620f48031b
Wade Christensen <wade@astuteape.com>
2019-03-20 20:45:30 -0700

    Add HeroText component

---
+++ src/components/HeroText.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+import "../styles/components.css"
+
+const HeroText = () => {
+  return (
+    <p className="hero-text">
+      I'm a <span className="span-lvl-one">software developer</span>,
+      <br />
+      <span className="span-lvl-one indent-lvl-one">multimedia producer</span>,
+      and <span className="span-lvl-one">educator</span>
+      <br />
+      <span className="indent-lvl-two">navigating</span> the technology and
+      media industries. I like talking to folks, so{" "}
+      <span className="span-lvl-two">
+        <Link to="../contact">get in touch</Link>
+      </span>
+      .
+    </p>
+  )
+}
+
+export default HeroText
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import HeroText from "../components/HeroText"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Astute Ape</h1>
+    <h1>Wade Christensen</h1>
+    <HeroText />
   </Layout>
 )
 
