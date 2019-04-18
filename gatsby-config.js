7f62b51bc6be07299ed512f1de7621865f233a96
Wade Christensen <wade@astuteape.com>
2019-04-18 14:58:05 -0700

    Add syntax highlighting to Markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,23 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: `language-`,
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
