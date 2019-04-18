b2175b98b8a94e64cac14b901d51f01289417551
Wade Christensen <wade@astuteape.com>
2019-04-18 16:24:28 -0700

    Organize color theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,2 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
 import "./src/styles/base.css"
-import "./src/styles/layout.css"
-import "./src/styles/state.css"
-import "./src/styles/modules.css"
 import "./src/styles/prism-xonokai.css"
