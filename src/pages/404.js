d87a6109c97090a3d7c044c18a78e6d3f7f1daff
Wade Christensen <wade@astuteape.com>
2019-05-15 16:19:25 -0700

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,15 +3,23 @@ import SEO from "../components/meta_data/seo"
 import PageTitle from "../components/text/page_title"
 import { Link } from "gatsby"
 
+// Site components
+import ApeIllustration from "../components/image/ape"
+
 const NotFoundPage = () => (
-  <main>
+  <div>
     <SEO title="404: Not found" />
-    <PageTitle titleText="404" />
-    <p>
-      Oops! This is a404br oken page. It's getting daaaark in here . . . .
-      Please take me <Link to="/">home</Link>.
-    </p>
-  </main>
+    <div className="index-container">
+      <PageTitle titleText="404" />
+      <ApeIllustration />
+      <div className="index-text-body">
+        <h2>
+          Oops! It looks like we're in uncharted lands. Let's go{" "}
+          <Link to="/">home</Link>.
+        </h2>
+      </div>
+    </div>
+  </div>
 )
 
 export default NotFoundPage
