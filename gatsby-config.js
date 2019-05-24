904d0b224f246f27c2b42cce59cdcc7a59b4d3c7
Wade Christensen <wade@astuteape.com>
2019-05-24 12:02:09 -0700

    Fix writing page description

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `blog`,
+        name: `writing`,
         path: `${__dirname}/content/writing`,
       },
     },
