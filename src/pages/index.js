ef6250d41aaf865673dd6b7ebd1ac6f336eb754f
Wade Christensen <wade@astuteape.com>
2019-05-24 22:34:31 -0700

    Clean up index page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import SEO from "../components/meta_data/seo"
 
 // Site components
 import ApeIllustration from "../components/image/ape"
-import DarkMode from "../components/theme/dark_mode"
 import HeroText from "../components/text/hero_text"
 import Layout from "../components/layout/layout"
 import PageTitle from "../components/text/page_title"
@@ -31,9 +30,7 @@ const IndexPage = () => (
     {/* Apply content layout rules to outermost div *-container */}
     <div className="index-container">
       <div>
-        <PageTitle titleText="Wade Christensen" />
-        <h3 className="page-title-aside">Is Astute Ape</h3>
-        <DarkMode />
+        <PageTitle titleText="Astute Ape" />
       </div>
       <ApeIllustration />
       <div className="index-text-body">
