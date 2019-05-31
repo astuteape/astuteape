76a03a78ba17ad1a17f7589843f5067663195e65
Wade Christensen <wade@astuteape.com>
2019-05-30 20:50:26 -0700

    Update contact form

--- src/components/forms/contact.js
+++ src/components/forms/contact.js
@@ -1,49 +1,95 @@
 import React from "react"
+import { navigate } from "gatsby-link"
 
-// Forms use Netlify Forms
-const ContactForm = () => (
-  <div>
-    <form
-      name="contact"
-      method="POST"
-      data-netlify="true"
-      netlify-honeypot="bot-field"
-      action="/form-success"
-    >
-      <p class="hidden">
-        <label>
-          All humans ignore this field: <input name="bot-field" />
-        </label>
-      </p>
-      <p>
-        <label>Full Name:</label>
-        <br />
-        <input type="text" name="name" placeholder="Nim Chimpsky" required />
-      </p>
-      <p>
-        <label>Email:</label>
-        <br />
-        <input
-          type="email"
-          name="email"
-          placeholder="nim@astuteape.com"
-          required
-        />
-      </p>
-      <p>
-        <label>Message:</label>
-        <br />
-        <textarea
-          name="message"
-          placeholder="Hi Wade, I have a question for you . . . ."
-          required
-        />
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  </div>
-)
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isValidated: false }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <div>
+        <form
+          name="contact"
+          method="post"
+          action="/form-success"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <div hidden>
+            <label>
+              All humans ignore this field:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </div>
+          <p>
+            <label htmlFor={"name"}>Full Name:</label>
+            <br />
+            <input
+              type={"text"}
+              name={"name"}
+              onChange={this.handleChange}
+              placeholder={"Nim Chimpsky"}
+              required={true}
+            />
+          </p>
+          <p>
+            <label htmlFor={"email"}>Email:</label>
+            <br />
+            <input
+              type={"email"}
+              name={"email"}
+              onChange={this.handleChange}
+              placeholder={"nim@astuteape.com"}
+              required={true}
+            />
+          </p>
+          <p>
+            <label htmlFor={"message"}>Message:</label>
+            <br />
+            <textarea
+              name={"message"}
+              onChange={this.handleChange}
+              placeholder={"Hi Wade, I have a question . . . ."}
+              required={true}
+            />
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default ContactForm
